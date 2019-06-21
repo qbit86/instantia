94eb115fa390f8d062a04a8bd60cb0f05ad5b01c
Viktor T <qbit86@gmail.com>
2019-06-21 22:24:26 +0300

    Implement basic ctor

--- src/Instantia/UtcDateTime.cs
+++ src/Instantia/UtcDateTime.cs
@@ -1,6 +1,23 @@
-﻿namespace Instantia
+using System;
+
+namespace Instantia
 {
     public readonly struct UtcDateTime
     {
+        private static readonly DateTime s_defaultDateTime = new DateTime(0, DateTimeKind.Utc);
+
+        private readonly DateTime _dateTime;
+
+        public UtcDateTime(DateTime dateTime)
+        {
+            if (dateTime.Kind != DateTimeKind.Utc)
+                throw new ArgumentException("Invalid DateTimeKind value.", nameof(dateTime));
+
+            _dateTime = dateTime;
+        }
+
+        public bool IsDefault => _dateTime.Kind == default;
+
+        public DateTime Value => IsDefault ? s_defaultDateTime : _dateTime;
     }
 }
