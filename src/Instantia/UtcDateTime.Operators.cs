e920a56c7db3ef016224ada686f23f296daf7ceb
Viktor T <qbit86@gmail.com>
2019-06-30 01:17:29 +0300

    Change expression body to block body for operators

--- src/Instantia/UtcDateTime.Operators.cs
+++ src/Instantia/UtcDateTime.Operators.cs
@@ -4,26 +4,54 @@ namespace Instantia
 {
     public readonly partial struct UtcDateTime
     {
-        public static UtcDateTime operator +(UtcDateTime d, TimeSpan t) =>
-            new UtcDateTime(d.ToDateTime().Add(t), false);
+        public static UtcDateTime operator +(UtcDateTime d, TimeSpan t)
+        {
+            return new UtcDateTime(d.ToDateTime().Add(t), false);
+        }
 
-        public static bool operator ==(UtcDateTime d1, UtcDateTime d2) => d1.Ticks == d2.Ticks;
+        public static bool operator ==(UtcDateTime d1, UtcDateTime d2)
+        {
+            return d1.Ticks == d2.Ticks;
+        }
 
-        public static bool operator !=(UtcDateTime d1, UtcDateTime d2) => d1.Ticks != d2.Ticks;
+        public static bool operator !=(UtcDateTime d1, UtcDateTime d2)
+        {
+            return d1.Ticks != d2.Ticks;
+        }
 
-        public static bool operator <(UtcDateTime t1, UtcDateTime t2) => t1.Ticks < t2.Ticks;
+        public static bool operator <(UtcDateTime t1, UtcDateTime t2)
+        {
+            return t1.Ticks < t2.Ticks;
+        }
 
-        public static bool operator <=(UtcDateTime t1, UtcDateTime t2) => t1.Ticks <= t2.Ticks;
+        public static bool operator <=(UtcDateTime t1, UtcDateTime t2)
+        {
+            return t1.Ticks <= t2.Ticks;
+        }
 
-        public static bool operator >(UtcDateTime t1, UtcDateTime t2) => t1.Ticks > t2.Ticks;
+        public static bool operator >(UtcDateTime t1, UtcDateTime t2)
+        {
+            return t1.Ticks > t2.Ticks;
+        }
 
-        public static bool operator >=(UtcDateTime t1, UtcDateTime t2) => t1.Ticks >= t2.Ticks;
+        public static bool operator >=(UtcDateTime t1, UtcDateTime t2)
+        {
+            return t1.Ticks >= t2.Ticks;
+        }
 
-        public static TimeSpan operator -(UtcDateTime d1, UtcDateTime d2) => new TimeSpan(d1.Ticks - d2.Ticks);
+        public static TimeSpan operator -(UtcDateTime d1, UtcDateTime d2)
+        {
+            return new TimeSpan(d1.Ticks - d2.Ticks);
+        }
 
-        public static UtcDateTime operator -(UtcDateTime d, TimeSpan t) =>
-            new UtcDateTime(d.ToDateTime().Subtract(t), false);
+        public static UtcDateTime operator -(UtcDateTime d, TimeSpan t)
+        {
+            return new UtcDateTime(d.ToDateTime().Subtract(t), false);
+        }
 
-        public static explicit operator DateTime(UtcDateTime d) => d.ToDateTime();
+        public static explicit operator DateTime(UtcDateTime d)
+        {
+            return d.ToDateTime();
+        }
     }
 }
