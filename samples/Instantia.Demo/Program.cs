cfe8cdd2abc354029476b226cdef1bd8f16e77ab
Viktor T <qbit86@gmail.com>
2019-06-30 21:49:53 +0300

    Add Instantia.Demo

---
+++ samples/Instantia.Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Instantia
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            DateTime localNow = DateTime.Now;
+            Debug.Assert(localNow.Kind == DateTimeKind.Local);
+
+            // Compile-time error: no implicit conversion from 'System.DateTime' to 'Instantia.UtcDateTime'.
+            // UtcDateTime utcNow = localNow;
+
+            // Run-time exception: invalid DateTimeKind value Local.
+            // UtcDateTime utcNow = new UtcDateTime(localNow);
+
+            // Valid explicit type conversion from 'System.DateTime' to 'Instantia.UtcDateTime'.
+            UtcDateTime utcNow = UtcDateTime.FromDateTime(localNow);
+            TakeUtcDateTime(utcNow);
+
+            // There is implicit conversion from 'Instantia.UtcDateTime' to 'System.DateTime'.
+            TakePlainDateTime(utcNow);
+        }
+
+        private static void TakeUtcDateTime(UtcDateTime instant)
+        {
+            Console.WriteLine(
+                $"[{nameof(TakeUtcDateTime)}] {nameof(instant)}: {instant}, {nameof(instant.Kind)}: {instant.Kind}");
+
+            DateTime underlyingDateTime = instant.ToDateTime();
+            Debug.Assert(underlyingDateTime.Kind == DateTimeKind.Utc);
+        }
+
+        private static void TakePlainDateTime(DateTime instant)
+        {
+            Console.WriteLine(
+                $"[{nameof(TakePlainDateTime)}] {nameof(instant)}: {instant}, {nameof(instant.Kind)}: {instant.Kind}");
+        }
+    }
+}
